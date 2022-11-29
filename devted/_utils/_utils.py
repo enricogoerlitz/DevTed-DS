350c7b40b895a2bd316288d6bc34ec3c38654fef
Enrico Goerlitz <enricogoerlitz@MacBook-Air-von-Enrico.local>
2022-11-29 08:01:36 +0100

    plot utils adde

--- devted/_utils/_utils.py
+++ devted/_utils/_utils.py
@@ -1,13 +1,29 @@
 """
 General utils
 """
+import devted.const as dtconst
+
+from typing import Union
 
 
 def zero_prefix(number_text: str) -> str:
     return f"0{number_text}" if int(number_text) < 10 else f"{number_text}"
 
-# def hrstr(minutes, suffix="h", usfmt=False, milliseconds=False)
-# def get_hours_string(minutes: float | int) -> str:
-#     hour_string  = zero_prefix(int(minutes / HOUR_MINUTES))
-#     minutes_string = zero_prefix(int(minutes % HOUR_MINUTES))
-#     return f"{hour_string}:{minutes_string}h"
+
+def hrstr(
+    minutes: Union[float, int],
+    suffix: str = "h",
+    milliseconds: bool = False
+) -> str:
+    misec = zero_prefix(int(minutes) - minutes)
+    hour_string = zero_prefix(int(minutes / dtconst.HOUR_MINUTES))
+    minutes_string = zero_prefix(int(minutes % dtconst.HOUR_MINUTES))
+    result_hrstr = (
+        f"{hour_string}:{minutes_string}{suffix}" if not milliseconds else
+        f"{hour_string}:{minutes_string}:{misec}{suffix}"
+    )
+    return result_hrstr
+
+
+# 15:05h
+# 15:05:30h
